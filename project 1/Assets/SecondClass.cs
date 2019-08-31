adc2a6678a79adac3f8bd464d7cc602b80b709af
Matt Poast <matt.poast@gmail.com>
2019-08-30 18:02:54 -0600

    I think i broke it

--- project 1/Assets/SecondClass.cs
+++ project 1/Assets/SecondClass.cs
@@ -2,17 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class secondscript : MonoBehaviour
+public class SecondClass : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+ 
+   
     {
-        
+
     }
 }
