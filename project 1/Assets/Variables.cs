3bcd61ecb92d5fad1bfd7f251d2ebfc7d80bce26
Matt Poast <matt.poast@gmail.com>
2019-09-05 15:12:31 -0600

    figuring out variables in unity

--- project 1/Assets/Variables.cs
+++ project 1/Assets/Variables.cs
@@ -11,15 +11,22 @@ public int intValue = 20;
 public string stringValue = "bob";
 public int firePower;
 public UnityEvent Event;
+
 private void OnTriggerEnter(Collider other)
 {
     Event.Invoke();
 
 }
 
+private void OnParticleTrigger()
+{
+    Event.Invoke();
+}
+
 // Start is called before the first frame update
     void Start()
     {
+        print("Hello World");
     }
 
     // Update is called once per frame
