97d29b010f04cc52e8d20af837b8bf2393eeb4ca
Matt Poast <matt.poast@gmail.com>
2019-09-12 15:49:30 -0600

    creating scrips and events

---
+++ project 1/Assets/MouseEvents.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseEvents : MonoBehaviour
+{
+   public ParticleSystem mouseDown;
+
+   private void OnMouseDown()
+   {
+      mouseDown.Emit(100);
+   }
+}
---
+++ project 1/Assets/WooHoo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WooHoo : MonoBehaviour
+{
+    public ParticleSystem woop;
+
+    private void OnMouseDown()
+    {
+        woop.Emit(50);
+    }
+}
