c5d044d1a51d5925cde36be0d92f9fb98da121ca
Matt Poast <matt.poast@gmail.com>
2019-09-17 15:54:23 -0600

    Character controllers in unity

---
+++ project 1/Assets/Movment.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Movment : MonoBehaviour
+{
+    public float gravity = -3f;
+    public float speed = 1f;
+   public  float jumpSpeed= 10f;
+   
+    private Vector3 position;
+    public CharacterController controller;
+    void Update()
+    {
+        //transform.Translate(position*Time.deltaTime);
+        position.x = speed*Input.GetAxis("Horizontal");
+        if (!controller.isGrounded)
+        { 
+            position.y += gravity;
+        }
+        else
+        {
+            position.y = 0f;
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            position.y = jumpSpeed;
+        }
+        
+        controller.Move(position*Time.deltaTime);
+    }
+}
