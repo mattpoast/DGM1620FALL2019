0921be8e377e2647446764c490809964127afcb7
Matt Poast <matt.poast@gmail.com>
2019-09-24 15:33:10 -0600

    If Else statements in unity

---
+++ project 1/Assets/IfStatments.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+public class IfStatments : MonoBehaviour
+{
+    public bool appleIsRipe, orangeIsRipe, peachIsRipe;
+
+    void Start()
+    {
+
+        if (appleIsRipe)
+        {
+            print("Go Pick The Apple.");
+        }
+        else if (orangeIsRipe)
+        {
+            print("Go Pick The Orange.");
+        }
+        else if (peachIsRipe)
+        {
+            print("Go Pick The Peach.");
+        }
+        else
+        {
+            print("Nothing is Ripe.");
+        }
+    }
+}
