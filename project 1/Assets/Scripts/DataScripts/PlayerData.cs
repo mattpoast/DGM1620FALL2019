87062e2dc7861e4fd253d31539ab393c9bcd2164
Matt Poast <matt.poast@gmail.com>
2019-10-10 15:46:30 -0600

    More Player Data

---
+++ project 1/Assets/Scripts/DataScripts/PlayerData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class PlayerData : GameArtData
+{
+    public FloatData health;
+    public ClothesData shirt;
+    public ClothesData pants;
+    public List<WeaponData> weapons;
+}
+
+
