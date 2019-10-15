a81c951f85c284227c88d4564e4e142d358aabe9
Matt Poast <matt.poast@gmail.com>
2019-10-15 15:55:00 -0600

    Player data and instantiating art

--- project 1/Assets/Scripts/DataScripts/PlayerData.cs
+++ project 1/Assets/Scripts/DataScripts/PlayerData.cs
@@ -7,6 +7,14 @@ public class PlayerData : GameArtData
     public ClothesData shirt;
     public ClothesData pants;
     public List<WeaponData> weapons;
+
+    public void InstancePlayer()
+    {
+        var newPlayer = Instantiate(prefab);
+        var playerSprite = newPlayer.GetComponentInChildren<SpriteRenderer>();
+        playerSprite.sprite = sprite;
+        playerSprite.color = spriteColor;
+    }
 }
 
 
