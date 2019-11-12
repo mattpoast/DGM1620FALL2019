f4c453d31cff6904dd1b058673a5bf38bd45d58e
Matt Poast <matt.poast@gmail.com>
2019-11-12 15:34:59 -0700

    CoRoutines

---
+++ project 1/Assets/Scripts/CoRoutines.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoRoutines : MonoBehaviour
+{
+    private int i = 3;
+    IEnumerator Start()
+    {
+        while (i > 0)
+        {
+            yield return new WaitForSeconds(1f);
+            Debug.Log(i);
+            i--;
+        } 
+        yield return new WaitForSeconds(1f);
+        Debug.Log("GO!");
+}
+
+}
