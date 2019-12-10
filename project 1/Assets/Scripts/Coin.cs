4d2d59b7cb6ecd3bf4f0e0675b75c148408a9969
matt poast <matt.poast@gmail.com>
2019-12-10 11:33:20 -0700

    Coin Collection script
    tutorial video

---
+++ project 1/Assets/Scripts/Coin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+   public int coinValue = 1;
+
+   private void OnTriggerEnter(Collider other)
+   {
+      if (other.gameObject.CompareTag("Player"))
+      {
+         CoinCollection.instance.ChangeScore(coinValue);
+      }
+   }
+}
---
+++ project 1/Assets/Scripts/CoinCollection.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinCollection : MonoBehaviour
+{
+    public static CoinCollection instance;
+    public Text scoreText;
+    private int score;
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    public void ChangeScore(int coinValue)
+    {
+        score += coinValue;
+        scoreText.text = "X" + score.ToString();
+    }
+}
+
