2fae4f065b6b607d2bd85a2a5833c3e9bb1ab185
matt poast <matt.poast@gmail.com>
2019-12-12 11:56:40 -0700

    Point and click script, new progect

---
+++ New Unity Project/Assets/scripts/PointAndClick.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class PointAndClick : MonoBehaviour
+{
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray click = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+
+            if (Physics.Raycast(click, out hit, 100.0f))
+            {
+                if (hit.transform)
+                {
+                    PrintName(hit.transform.gameObject);
+                }
+            }
+        }
+
+         void PrintName(GameObject go)
+        {
+            print(go.name);
+
+        }
+
+
+    }
+}
