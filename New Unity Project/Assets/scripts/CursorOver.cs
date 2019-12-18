3cd1857d6e5bd9bb418d708cc90b3b397031ba7c
matt poast <matt.poast@gmail.com>
2019-12-17 20:40:35 -0700

    Cursor over objects script!

---
+++ New Unity Project/Assets/scripts/CursorOver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+public class CursorOver : MonoBehaviour
+{
+ public Texture2D defaultMouse;
+ public Texture2D objectMouse;
+public Vector2 hotSpot = Vector2.zero;
+public CursorMode curMode = CursorMode.Auto;
+
+private void Start()
+{
+ Cursor.SetCursor(defaultMouse, hotSpot, curMode);
+}
+
+private void OnMouseEnter()
+ {
+  if (gameObject.tag == "objectCursor")
+  {
+   Cursor.SetCursor(objectMouse, hotSpot, curMode);
+  }
+ }
+
+ private void OnMouseExit()
+ {
+  Cursor.SetCursor(defaultMouse, hotSpot, curMode);
+ }
+}
+
+    
+    
+
+
