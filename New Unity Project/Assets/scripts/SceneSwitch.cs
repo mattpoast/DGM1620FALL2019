5db05105636386b49825bde21ce2ef273b771a11
matt poast <matt.poast@gmail.com>
2019-12-21 17:55:57 -0800

    Script to Switch scenes

---
+++ New Unity Project/Assets/scripts/SceneSwitch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class SceneSwitch : MonoBehaviour
+{
+    public bool sceneSwitch;
+    
+    private void OnMouseDown(SceneSwitch)
+    {
+        if (sceneSwitch = true)
+        {
+            CutsceneStart
+        }
+    }
+}
