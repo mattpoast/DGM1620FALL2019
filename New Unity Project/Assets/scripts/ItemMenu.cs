6af9187f7f7affb6259356155f6062ce197bd8ec
matt poast <matt.poast@gmail.com>
2019-12-23 16:29:48 -0800

    script for pop up ui "item menu"

---
+++ New Unity Project/Assets/scripts/ItemMenu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemMenu : MonoBehaviour
+{
+ public Vector2 dbPosition;
+ public Image dashBoard;
+ public void OnMouseEnter()
+ {
+  dashBoard.transform.position = dbPosition;
+ }
+}
