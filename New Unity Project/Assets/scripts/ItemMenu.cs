b16f71330b2f3c59557e063d73e078ebab91e0f2
matt poast <matt.poast@gmail.com>
2019-12-27 12:24:53 -0800

    still working on item menu

--- New Unity Project/Assets/scripts/ItemMenu.cs
+++ New Unity Project/Assets/scripts/ItemMenu.cs
@@ -4,22 +4,22 @@ using UnityEngine.UI;
 
 public class ItemMenu : MonoBehaviour
 {
- public float seconds = 3f;
- private WaitForSeconds wfsObj;
  public GameObject dashBoard;
+ public GameObject menuButton;
  private void Start()
  {
   dashBoard.gameObject.SetActive(false);
+    menuButton.gameObject.SetActive(false);
  }
 
  public void OnMouseEnter()
  {
   dashBoard.gameObject.SetActive(true);
-  wfsObj = new WaitForSeconds(seconds);
+  menuButton.gameObject.SetActive(true);
  }
-
  private void OnMouseExit()
  {
   dashBoard.gameObject.SetActive(false);
+  menuButton.gameObject.SetActive(false);
  }
 }
