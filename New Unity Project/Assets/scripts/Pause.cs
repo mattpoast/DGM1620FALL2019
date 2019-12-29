76548055fce65b4c23045f438e45ae0cf150e8bc
matt poast <matt.poast@gmail.com>
2019-12-29 13:33:26 -0800

    pause function

---
+++ New Unity Project/Assets/scripts/Pause.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    private bool pause = false;
+
+    public void pauseGame()
+    {
+        if (pause)
+        {
+            Time.timeScale = 1;
+            pause = false;
+        }
+        else
+        {
+            Time.timeScale = 0;
+        }
+    }
+}
+
