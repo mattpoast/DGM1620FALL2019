4f8af2c91cea1dd87d930b69c8d94b36e0a08665
matt poast <matt.poast@gmail.com>
2020-01-08 23:52:37 -0700

    creating a start up screen

---
+++ New Unity Project/Assets/scripts/startScreen.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class startScreen : MonoBehaviour
+{
+
+    public GameObject screen;
+    void Start()
+    {
+        screen.gameObject.SetActive(true);
+    }
+
+    public void OnMouseDown()
+    {
+        screen.gameObject.SetActive(false);
+    }
+}
