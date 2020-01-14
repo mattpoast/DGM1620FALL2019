901ab2c1b024f86e56a943bba05179329d9840a8
matt poast <matt.poast@gmail.com>
2020-01-13 17:48:42 -0700

    script to change cursor while it is being "clicked"

---
+++ New Unity Project/Assets/scripts/ChangeCursor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class ChangeCursor : MonoBehaviour
+{
+    public Texture2D downClick;
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Cursor.SetCursor(downClick);
+    }
+}
