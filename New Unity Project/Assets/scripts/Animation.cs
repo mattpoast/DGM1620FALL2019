65a10ee4234c2b174b9ec2127dbc31d271fde3b9
matt poast <matt.poast@gmail.com>
2020-01-27 12:13:32 -0700

    attaching dumpster animation to script

---
+++ New Unity Project/Assets/scripts/Animation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    public Animation dumpster;
+    private void OnMouseDown()
+    {
+        GetComponent<Animation>();
+    }
+}
