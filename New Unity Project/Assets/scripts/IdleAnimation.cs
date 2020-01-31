d0f718203d5f083ae71c850789e2f80b36450aa0
matt poast <matt.poast@gmail.com>
2020-01-30 23:12:32 -0700

    working on script for idle animations/whateverelse

---
+++ New Unity Project/Assets/scripts/IdleAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class IdleAnimation : MonoBehaviour
+{
+    private float waiting;
+    public GameObject idleObject;
+
+
+    private void Start()
+    {
+        idleObject.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (waiting >= 7f)
+        {
+            idleObject.gameObject.SetActive(true);
+        }
+        else 
+        {
+            idleObject.gameObject.SetActive(false);
+        }
+
+    }
+}
