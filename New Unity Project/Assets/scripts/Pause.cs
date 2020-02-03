d6c7331dfffe6d92fa32aaef7233b74d602f2880
matt poast <matt.poast@gmail.com>
2020-02-02 23:22:24 -0700

    working on a better pause feature

--- New Unity Project/Assets/scripts/Pause.cs
+++ New Unity Project/Assets/scripts/Pause.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using UnityEditor;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Pause : MonoBehaviour
 {
---
+++ New Unity Project/Assets/scripts/Pause2.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Pause2 : MonoBehaviour
+{
+    public bool screen;
+
+    void Start()
+    {
+        screen = false;
+    }
+
+    void Update()
+    {
+        Input.GetKeyDown(KeyCode.Space);
+        {
+            screen = true;
+        }
+    }
+}
