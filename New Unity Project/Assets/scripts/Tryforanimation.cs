71e28f6b6ba67fcb77bd27f604e5f072c50d485f
matt poast <matt.poast@gmail.com>
2020-02-08 12:16:53 -0700

    videoClip play on mouse down

---
+++ New Unity Project/Assets/scripts/Tryforanimation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class Tryforanimation : MonoBehaviour
+{
+    public GameObject entrance;
+    public VideoClip animationn;
+
+    private void OnMouseDown()
+    { 
+        Instantiate(animationn);
+    }
+}
