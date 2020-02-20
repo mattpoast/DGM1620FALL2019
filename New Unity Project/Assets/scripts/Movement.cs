1410972c6edc7c7a06953e3cb55e1c384f08b5bb
matt poast <matt.poast@gmail.com>
2020-02-20 11:52:42 -0700

    movement script

---
+++ New Unity Project/Assets/scripts/Movement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+public class Movement : MonoBehaviour
+{
+    public float movmentSpeed;
+    void Update()
+    {
+        if (Input.GetKey("d"))
+        {
+            transform.position += transform.TransformDirection(Vector3.right) * Time.deltaTime * movmentSpeed;
+        }
+    }
+}
