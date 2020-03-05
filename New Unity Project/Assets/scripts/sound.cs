0b0a3da1ea442a42c0a383acdbe31fe116033798
matt poast <matt.poast@gmail.com>
2020-03-04 23:04:10 -0700

    sound script

---
+++ New Unity Project/Assets/scripts/sound.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Experimental.LowLevel;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class sound : MonoBehaviour
+{
+    public sound soundEffect;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            
+        }
+    }
+    
+    }
+
