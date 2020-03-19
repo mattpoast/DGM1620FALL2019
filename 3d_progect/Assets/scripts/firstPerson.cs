5662f2a1b203c34c44588b3c85094f818992a658
matt poast <matt.poast@gmail.com>
2020-03-18 23:34:50 -0600

    first person camera

---
+++ 3d_progect/Assets/scripts/firstPerson.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firstPerson : MonoBehaviour
+{
+    public Camera woop;
+  
+    void Update()
+    {
+        
+    }
+}
