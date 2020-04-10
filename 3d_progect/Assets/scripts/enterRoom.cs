440ef45f5902ba7c1fc690e237aaa2da7c8eb4ca
matt poast <matt.poast@gmail.com>
2020-04-09 22:17:42 -0600

    load new scene on collison

---
+++ 3d_progect/Assets/scripts/enterRoom.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class enterRoom : MonoBehaviour
+{
+    public GameObject door;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(1);
+    }
+}
+
