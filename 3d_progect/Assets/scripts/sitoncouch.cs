aa46f9b1c8ce748ce9511052eb59e49fadbc15d9
matt poast <matt.poast@gmail.com>
2020-04-13 23:00:32 -0600

    script to move transformposition of player when collides

---
+++ 3d_progect/Assets/scripts/sitoncouch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sitoncouch : MonoBehaviour
+{
+    public Vector3 position;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        position.x = +1;
+    }
+}
