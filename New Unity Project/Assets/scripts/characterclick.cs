6dc2672b6afdf7c17c9e2c16c2111f82866fd81b
matt poast <matt.poast@gmail.com>
2020-04-23 23:14:34 -0600

    script to move 3d character on mouse down

---
+++ New Unity Project/Assets/scripts/characterclick.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class characterclick : MonoBehaviour
+{
+    public Vector3 turn;
+    public GameObject button;
+    private void OnMouseDown()
+    {
+        Vector3 rotateTowards;
+        rotateTowards = Vector3.RotateTowards(current: button);
+    }
+}
